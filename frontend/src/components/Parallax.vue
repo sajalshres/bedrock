7d5585ecddd953a61bf198c02964876fba15c2fb
Sajal Shrestha <sajal.shres@gmail.com>
2020-05-26 13:43:07 +0000

    Added parallax component

---
+++ frontend/src/components/Parallax.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-parallax dark src="../assets/parallax-home.jpg">
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-3 font-weight-thin mb-4">CornerStone</h1>
+        <h4 class="subheading">When Infrastructure meets Development</h4>
+        <h4 class="caption">
+          More features coming along, including this dashboard
+        </h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
