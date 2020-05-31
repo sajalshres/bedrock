b08527cfd17feaa5287228a1db28afd7a1064974
Sajal Shrestha <sajal.shres@gmail.com>
2020-05-31 22:10:31 +0545

    Test failed unit tests

--- bedrock/sor/tests/test_views.py
+++ bedrock/sor/tests/test_views.py
@@ -18,6 +18,11 @@ from sor.models import (
 )
 
 
+class FailedTestCase(APITestCase):
+    def test_fail_a_test(self):
+        self.assertFalse(True)
+
+
 class LabelViewTestCase(APITestCase):
     @classmethod
     def setUpTestData(cls):
