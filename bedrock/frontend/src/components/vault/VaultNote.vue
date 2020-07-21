b32a4119e71d2409b37deabe9fc6312cf80ce3fa
Sajal Shrestha <sajal.shres@gmail.com>
2020-07-21 07:58:43 +0000

    Updated vault frontend

---
+++ bedrock/frontend/src/components/vault/VaultNote.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-row>
+    <v-dialog v-model="dialog" max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn text v-bind="attrs" v-on="on">
+          <v-icon left>mdi-note-text</v-icon>
+          {{ vaultNote.name }}
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit: {{ vaultNote.name }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="vaultNote.name"
+                  label="Name*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-autocomplete
+                  :items="[
+                    'Skiing',
+                    'Ice hockey',
+                    'Soccer',
+                    'Basketball',
+                    'Hockey',
+                    'Reading',
+                    'Writing',
+                    'Coding',
+                    'Basejump',
+                  ]"
+                  label="Labels"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea label="Notes"></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false"
+            >Close</v-btn
+          >
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    vaultNote: Object,
+  },
+  data: () => ({
+    data: false,
+    dialog: false,
+  }),
+};
+</script>
+
+<style></style>
--- bedrock/frontend/src/views/Vault.vue
+++ bedrock/frontend/src/views/Vault.vue
@@ -1,16 +1,114 @@
 <template>
   <v-container fluid>
     <BreadCrumbs />
+    <v-data-table
+      :headers="headers"
+      :items="vault"
+      :search="search"
+      :dense="dense"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat class="pt-2">
+          <v-text-field
+            v-model="search"
+            prepend-inner-icon="mdi-magnify"
+            label="Search"
+            clearable
+            hide-details
+            class="pb-1"
+          ></v-text-field>
+          <v-spacer />
+          <v-btn class="mx-2" depressed fab small @click.stop>
+            <v-icon dark>mdi-plus</v-icon>
+          </v-btn>
+          <v-btn class="mx-2" depressed fab small @click.stop>
+            <v-icon dark>mdi-reload</v-icon>
+          </v-btn>
+          <v-switch class="mx-2 pt-4" label="Small" v-model="dense"></v-switch>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.name="{ item }">
+        <VaultItem v-if="item.type === 'item'" :vaultItem="item" />
+        <VaultLogin v-else-if="item.type === 'login'" :vaultLogin="item" />
+        <VaultNote v-else-if="item.type === 'note'" :vaultNote="item" />
+      </template>
+      <template v-slot:item.labels="{ item }">
+        <v-chip
+          label
+          small
+          class="ml-2 my-2"
+          v-for="label in item.labels"
+          :key="label"
+          >{{ label }}</v-chip
+        >
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
 <script>
 import BreadCrumbs from '../components/BreadCrumbs';
+import VaultItem from '../components/vault/VaultItem';
+import VaultLogin from '../components/vault/VaultLogin';
+import VaultNote from '../components/vault/VaultNote';
 
 export default {
   name: 'Vault',
   components: {
     BreadCrumbs,
+    VaultItem,
+    VaultLogin,
+    VaultNote,
+  },
+  data: () => ({
+    resourceType: 'vault',
+    loadingText: 'Loading vault, please wait.',
+    dense: false,
+    dialog: false,
+    expanded: [],
+    singleExpand: true,
+    search: '',
+    headers: [
+      { text: 'Name', value: 'name', align: 'start', width: '40%' },
+      { text: 'Type', value: 'type', align: 'end' },
+      { text: 'Labels', value: 'labels', align: 'end' },
+    ],
+  }),
+  created() {
+    this.initialize();
+  },
+  methods: {
+    initialize() {
+      this.vault = [
+        {
+          name: 'Bedrock Version',
+          type: 'item',
+          value: '0.1',
+          labels: ['production', 'version'],
+        },
+        {
+          name: 'Bedrock CLI Version',
+          type: 'item',
+          value: '0.1',
+          labels: ['production', 'version'],
+        },
+        {
+          name: 'Github',
+          type: 'login',
+          username: 'testUser',
+          password: 'da3132321!#$',
+          link: 'https://www.github.com',
+          labels: ['dev'],
+        },
+        {
+          name: 'Github SSH KEY',
+          type: 'note',
+          note: 'kasdjsakldjlkdjaksljdq9249274239021-02323@!#*@)($&*@)(',
+          labels: ['dev'],
+        },
+      ];
+    },
   },
 };
 </script>
