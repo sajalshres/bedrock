841e1ce2f91de7181ea5f120e6d2c0b6873a6327
Sajal Shrestha <sajal.shres@gmail.com>
2020-08-10 12:41:16 +0000

    added documentaiton api

--- bedrock/config/urls.py
+++ bedrock/config/urls.py
@@ -14,9 +14,42 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, re_path, include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from config.views import HomePageView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BedRock API",
+        default_version="v1",
+        description="Brings together infrastructure and development",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="sajal.shres@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=False,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
     path("api/auth/", include("authenticate.urls")),
     path("api/sor/", include("sor.urls")),
     path("api/vault/", include("vault.urls")),
